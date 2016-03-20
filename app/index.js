2cd19a8dca87fdfbaa374852e336e8cac90f62d5
RBKrishna <rayala.scientist@gmail.com>
2016-03-20 17:51:26 +0530

    add package motivations

--- app/index.js
+++ app/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var motivations = require('motivations');
+
 app.get('/', function(request, response){
-	response.send("hii evryone");
+	//response.send("hii evryone");
+	response.send(motivations[9]);
 });
 
 module.exports = app;
