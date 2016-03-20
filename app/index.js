e19fb1c523976882f8f47faf088fec63962e727d
RBKrishna <rayala.scientist@gmail.com>
2016-03-20 17:56:56 +0530

    pickonepackage

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 var motivations = require('motivations');
+var pickone = require('pick-one');
 
 app.get('/', function(request, response){
 	//response.send("hii evryone");
-	response.send(motivations[9]);
+	//response.send(motivations[9]);
+	response.send(pickone(motivations));
 });
 
 module.exports = app;
